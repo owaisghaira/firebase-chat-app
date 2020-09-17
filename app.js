017646429d6499dc978d9cf0f964855e8c993ddd
owaisghaira <owais.ghaira99@gmail.com>
2020-09-17 14:02:29 +0500

    chat app database  done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 firebase.database().ref("sms").on('child_added',function(data){
-    console.log(data.val().value)
+    // console.log(data.val().value)
+    var cheekUser = data.val().user
+    if (cheekUser === "user1"){
+    console.log(cheekUser)
     var spn = document.createElement("span")
         var li = document.createElement("li")
         var text = document.createTextNode(data.val().value)
@@ -8,6 +11,17 @@ firebase.database().ref("sms").on('child_added',function(data){
         li.setAttribute("class","left")
         li.appendChild(spn)
         render.appendChild(li)
+    }
+    else{
+        var spn = document.createElement("span")
+        var li = document.createElement("li")
+        var text = document.createTextNode(data.val().value)
+        spn.appendChild(text)
+        spn.setAttribute("class","hello")
+        li.setAttribute("class","right")
+        li.appendChild(spn)
+        render.appendChild(li)
+    }
 })
 // firebase.database().ref("sms").on('child_added',function(data){
 //     console.log(data.val().value)
@@ -21,7 +35,7 @@ firebase.database().ref("sms").on('child_added',function(data){
 //         render.appendChild(li)
 // })
 
-var user = user1
+var user = 'user1';
 let show = ()=>{
     var item = document.getElementById("msg");
 //     var render = document.getElementById("render");
