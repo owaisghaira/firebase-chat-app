cef692200f9b8981ae8b53923c18fb3b318867ec
owaisghaira <owais.ghaira99@gmail.com>
2020-09-17 13:46:11 +0500

    chat app ui done

---
+++ app.js
@@ -0,0 +1,36 @@
+firebase.database().ref("sms").on('child_added',function(data){
+    console.log(data.val().value)
+    var spn = document.createElement("span")
+        var li = document.createElement("li")
+        var text = document.createTextNode(data.val().value)
+        spn.appendChild(text)
+        spn.setAttribute("class","hello")
+        li.setAttribute("class","left")
+        li.appendChild(spn)
+        render.appendChild(li)
+})
+// firebase.database().ref("sms").on('child_added',function(data){
+//     console.log(data.val().value)
+//     var spn = document.createElement("span")
+//         var li = document.createElement("li")
+//         var text = document.createTextNode(data.val().value)
+//         spn.appendChild(text)
+//         spn.setAttribute("class","hello")
+//         li.setAttribute("class","right")
+//         li.appendChild(spn)
+//         render.appendChild(li)
+// })
+
+var user = user1
+let show = ()=>{
+    var item = document.getElementById("msg");
+//     var render = document.getElementById("render");
+var key = firebase.database().ref().push().key
+var msg = {
+    key:key,
+    user:user,
+    value:item.value
+}
+firebase.database().ref('sms').child(key).set(msg)  
+    item.value = "";
+ }
